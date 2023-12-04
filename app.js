4724bbab542767384d339f955011350b9b930eae
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-04 16:27:07 +0100

    CREATE and GET users endpoints created

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require("express");
+const { v4: uuid } = require("uuid")
 const app = express();
 
 let blogs = []
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
+app.get("/users", (req, res) => {
     res.send(blogs)
 })
 
-app.get("/:id", (req, res) => {
+app.get("/users/:id", (req, res) => {
     const { id } = req.params;
     const blog = blogs.find((blog) => blog.id === id)
     res.send(blog)
+})
+
+app.post("/users", (req, res) => {
+    const id = uuid();
+    const body = req.body;
+    const updated_body = { ...body, id: id }
+    blogs.push(updated_body);
+    res.send(blogs)
 })
 
 app.listen(3000, () => {
