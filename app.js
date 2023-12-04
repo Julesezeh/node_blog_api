9240cde13fb2dd10ef2b58c5971ad08e99b973c8
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-04 16:19:15 +0100

    The first two Read methods created

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+let blogs = []
 
+app.use(express.json())
 
+app.get("/", (req, res) => {
+    res.send(blogs)
+})
+
+app.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const blog = blogs.find((blog) => blog.id === id)
+    res.send(blog)
+})
 
 app.listen(3000, () => {
     console.log("server is listening on localhost:3000");
