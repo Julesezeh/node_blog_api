a7d58b8130b04312e212b22ecaf0fa3ff7ba7bf1
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-04 17:08:35 +0100

    debugging tools, middlware debugged successfully

--- userIDPassedMiddleWare.js
+++ userIDPassedMiddleWare.js
@@ -1,6 +1,10 @@
 function IsIdPresent(req, res, next) {
-    if (!req.params.id) {
+    const { id } = req.params
+    console.log("from le middleware")
+    console.log(id)
+    if (id.length < 12) {
         console.log("Please enter a valid ID")
+        res.status(400).send("<h1>Invalid ID</h1>")
     }
     next();
 }
